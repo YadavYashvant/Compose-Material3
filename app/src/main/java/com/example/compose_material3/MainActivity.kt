9cd7e34b0d7ebdcb005137d4238c8819b37e6cd9
YadavYashvant <yashvantyadav855@gmail.com>
2023-08-29 21:40:37 +0530

    used Material3 to build bottom bar

--- app/src/main/java/com/example/compose_material3/MainActivity.kt
+++ app/src/main/java/com/example/compose_material3/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.compose_material3
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_material3.ui.theme.Composematerial3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +44,35 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box {
+                    BottomAppBar (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                    ){
+                        IconButton(onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .padding(10.dp,5.dp)
+                                .size(ButtonDefaults.IconSize)
+                        ) {
+                            Icon(Icons.Filled.Check, contentDescription = "description",
+                                )
+                        }
+                        IconButton(onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .padding(10.dp,5.dp)
+                            ) {
+                            Icon(imageVector = Icons.Filled.Edit, contentDescription = "Edit")
+                        }
+                        ExtendedFloatingActionButton(onClick = { /*TODO*/ },
+                            containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                            ) {
+                            Icon(imageVector = Icons.Filled.Add, contentDescription = "checking")
+                        }
+                    }}
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Composematerial3Theme {
-        Greeting("Android")
     }
 }
